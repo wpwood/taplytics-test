5e0147481e604a544ad8f628e42f93fe0fedc25e
Bill Woodward <bill.woodward@block.one>
2020-09-02 15:46:20 -0400

    User the taplytics React SDK

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Taplytics from '@taplytics/js-sdk';
+
+Taplytics.init('53303f82931647ddb7c2eb43a3c50cc8');
+
+Taplytics.propertiesLoaded(function (loaded) {
+  console.log(`loaded = ${loaded}`)
+  Taplytics.runningFeatureFlags((runningFF) => {
+    console.log(JSON.stringify(runningFF))
+  })
+
+  if (Taplytics.featureFlagEnabled('iafWeb')) {
+    console.log('iafWeb is enabled')
+  } else {
+    console.log('iafWeb is not enabled')
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
